485ed7c8eda312179acc8162f9c391109c355ab0
lotuspar <91263142+lotuspar@users.noreply.github.com>
2023-03-25 01:51:29 +0800

    new namespace, shorten Game.cs, use global usings

--- code/Game.cs
+++ code/Game.cs
@@ -1,51 +1,28 @@
-﻿using Sandbox;
-using Sandbox.UI.Construct;
-using System;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿global using Sandbox;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
 
-//
-// You don't need to put things in a namespace, but it doesn't hurt.
-//
-namespace Sandbox;
+namespace GameJam;
 
-/// <summary>
-/// This is your game class. This is an entity that is created serverside when
-/// the game starts, and is replicated to the client. 
-/// 
-/// You can use this to create things like HUDs and declare which player class
-/// to use for spawned players.
-/// </summary>
-public partial class MyGame : GameManager
+public partial class GameMgr : GameManager
 {
-	public MyGame()
+	public GameMgr()
 	{
 	}
 
-	/// <summary>
-	/// A client has joined the server. Make them a pawn to play with
-	/// </summary>
 	public override void ClientJoined( IClient client )
 	{
 		base.ClientJoined( client );
 
-		// Create a pawn for this client to play with
 		var pawn = new Pawn();
 		client.Pawn = pawn;
 
-		// Get all of the spawnpoints
-		var spawnpoints = Entity.All.OfType<SpawnPoint>();
+		if ( All.OfType<SpawnPoint>().MinBy( x => Guid.NewGuid() ) is not { } spawn )
+			return;
 
-		// chose a random one
-		var randomSpawnPoint = spawnpoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
-
-		// if it exists, place the pawn there
-		if ( randomSpawnPoint != null )
-		{
-			var tx = randomSpawnPoint.Transform;
-			tx.Position = tx.Position + Vector3.Up * 50.0f; // raise it up
-			pawn.Transform = tx;
-		}
+		var transform = spawn.Transform;
+		transform.Position += Vector3.Up * 50.0f;
+		pawn.Transform = transform;
 	}
 }
