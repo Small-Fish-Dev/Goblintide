607be0b2f4879b8cef88e571b9c09384432ed4ca
ceitine <fluffy00gt@gmail.com>
2023-03-26 18:55:31 +0300

    gamestate system

--- code/Game.cs
+++ code/Game.cs
@@ -9,9 +9,25 @@ namespace GameJam;
 // TODO(gio): rename GameMgr when we have an actual idea! 
 public partial class GameMgr : GameManager
 {
+	/// <summary>
+	/// The singleton for GameManager.
+	/// </summary>
+	public static GameMgr Instance { get; private set; }
+
+	/// <summary>
+	/// The current GameState.
+	/// </summary>
+	public static GameState State => Instance.state;
+	[Net, Change( "gameStateChanged" )] private GameState state { get; set; }
+
 	public GameMgr()
 	{
+		Instance = this;
 		Game.TickRate = 30;
+
+		// Set the beginning state.
+		if ( Game.IsServer )
+			SetState<VillageState>();
 	}
 
 	public override void ClientJoined( IClient client )
@@ -27,5 +43,45 @@ public partial class GameMgr : GameManager
 		var transform = spawn.Transform;
 		transform.Position += Vector3.Up * 50.0f;
 		pawn.Transform = transform;
+	}
+
+	/// <summary>
+	/// Set the current GameState to T : GameState.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <returns>The new state if setting it was successful.</returns>
+	public static T SetState<T>() where T : GameState
+	{
+		// We shouldn't be setting the state on client.
+		Game.AssertServer();
+
+		var type = typeof( T );
+		if ( State != null && type == State?.GetType() )
+		{
+			Log.Error( "We don't want to set the same GameState twice." );
+			return null;
+		}
+
+		// Handle creating new state.
+		var newState = (T)TypeLibrary.Create( type.FullName, type );
+		if ( newState == null )
+			return null;
+
+		State?.Changed( newState );
+		newState.Initialize();
+
+		// Override the old state.
+		Instance.state = newState;
+		return newState;
+	}
+
+	// Handle changing states on client.
+	private static void gameStateChanged( GameState old, GameState current )
+	{
+		// Call changed.
+		old?.Changed( current );
+
+		// Call initialize.
+		current.Initialize();
 	}
 }
---
+++ code/GameState/GameState.cs
@@ -0,0 +1,33 @@
+﻿namespace GameJam;
+
+public abstract partial class GameState : BaseNetworkable
+{
+	public GameState()
+	{
+		// Register events for this GameState.
+		Event.Register( this );
+	}
+
+	~GameState()
+	{
+		// Unregister the events.
+		Event.Unregister( this );
+	}
+	
+	/// <summary>
+	/// Called when this state is created.
+	/// </summary>
+	public virtual void Initialize()
+	{
+
+	}
+
+	/// <summary>
+	/// Called when the state is changed to something else.
+	/// </summary>
+	/// <param name="state">The new state.</param>
+	public virtual void Changed( GameState state )
+	{
+
+	}
+}
