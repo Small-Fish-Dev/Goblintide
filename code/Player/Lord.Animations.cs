b154728e6d5957f9a88437fbbf44df348b5fe85f
lotuspar <91263142+lotuspar@users.noreply.github.com>
2023-03-27 23:55:36 +0800

    Slow down prev. commit a tad

--- code/Player/Lord.Animations.cs
+++ code/Player/Lord.Animations.cs
@@ -5,7 +5,7 @@ public partial class Lord
 	public void SimulateAnimations()
 	{
 		// note(gio): made the animations a bit more speedy - nicer to look at imo
-		SetAnimParameter( "move_x", Velocity.Dot( Rotation.Forward ) * 1.5f );
-		SetAnimParameter( "move_y", Velocity.Dot( Rotation.Right ) * 1.5f );
+		SetAnimParameter( "move_x", Velocity.Dot( Rotation.Forward ) * 1.35f );
+		SetAnimParameter( "move_y", Velocity.Dot( Rotation.Right ) * 1.35f );
 	}
 }
