b978afeca4cc73fc13f0bcacc8f032dc7ef69ee3
lotuspar <91263142+lotuspar@users.noreply.github.com>
2023-03-27 23:54:27 +0800

    Speed up animations - a bit more natural

--- code/Player/Lord.Animations.cs
+++ code/Player/Lord.Animations.cs
@@ -4,7 +4,8 @@ public partial class Lord
 {
 	public void SimulateAnimations()
 	{
-		SetAnimParameter( "move_x", Velocity.Dot( Rotation.Forward ) );
-		SetAnimParameter( "move_y", Velocity.Dot( Rotation.Right ) );
+		// note(gio): made the animations a bit more speedy - nicer to look at imo
+		SetAnimParameter( "move_x", Velocity.Dot( Rotation.Forward ) * 1.5f );
+		SetAnimParameter( "move_y", Velocity.Dot( Rotation.Right ) * 1.5f );
 	}
 }
