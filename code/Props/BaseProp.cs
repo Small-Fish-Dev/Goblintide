b6b86f720744293d900f6ab410aa1fa6f275b963
yuberee <59583743+yuberee@users.noreply.github.com>
2023-03-29 00:55:48 +0200

    Prop stacks, Prefab resource sliders

--- code/Props/BaseProp.cs
+++ code/Props/BaseProp.cs
@@ -7,6 +7,15 @@ public partial class BaseProp : BaseEntity
 	[Prefab, Category( "Stats" )]
 	public override float HitPoints { get; set; } = 0.5f;
 
+	[Prefab, Category( "Stats" )]
+	public virtual RangedFloat GoldDropped { get; set; } = new RangedFloat(0f, 0f);
+	[Prefab, Category( "Stats" )]
+	public virtual RangedFloat WoodDropped { get; set; } = new RangedFloat( 0f, 0f );
+	[Prefab, Category( "Stats" )]
+	public virtual RangedFloat FoodDropped { get; set; } = new RangedFloat( 0f, 0f );
+	[Prefab, Category( "Visual" )]
+	public virtual bool IsBreakable { get; set; } = true;
+
 	public BaseProp() { }
 
 	public override void Spawn()
@@ -25,8 +34,12 @@ public partial class BaseProp : BaseEntity
 
 	public override void Kill()
 	{
-		var result = new Breakables.Result();
-		Breakables.Break( this, result );
+
+		if ( IsBreakable )
+		{
+			var result = new Breakables.Result();
+			Breakables.Break( this, result );
+		}
 
 		base.Kill();
 	}
