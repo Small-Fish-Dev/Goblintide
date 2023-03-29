326d0706084b1f0ecb9b44112060ed8a23b6cd94
lotuspar <91263142+lotuspar@users.noreply.github.com>
2023-03-30 03:11:08 +0800

    `SingletonPanel` impl

---
+++ code/UI/Core/SingletonPanel.cs
@@ -0,0 +1,40 @@
+﻿using Sandbox.UI;
+
+namespace GameJam;
+
+public class SingletonPanel<T> : Panel where T : Panel, new()
+{
+	public static SingletonPanel<T> Instance { get; protected set; }
+
+	public static bool IsOpen => Instance != null;
+
+	public SingletonPanel()
+	{
+		Instance?.Delete( true );
+		Instance = this;
+	}
+
+	public override void OnDeleted()
+	{
+		base.OnDeleted();
+
+		Instance = null;
+	}
+
+	public static void Create( Panel parent )
+	{
+		if ( !IsOpen )
+			parent.AddChild( new T() );
+	}
+
+	public static void Toggle( Panel parent )
+	{
+		if ( IsOpen )
+			Instance.Delete();
+		else
+			Create( parent );
+	}
+
+	public static void Create() => Create( HUD.Instance );
+	public static void Toggle() => Toggle( HUD.Instance );
+}
