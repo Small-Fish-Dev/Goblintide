2511338a3f3906febef7ef1d95047dc79618e144
lotuspar <91263142+lotuspar@users.noreply.github.com>
2023-03-30 17:45:42 +0800

    Basic impl

---
+++ code/UI/SkillTree/PlayerView.cs
@@ -0,0 +1,28 @@
+﻿using Sandbox.UI;
+
+namespace GameJam.UI;
+
+public class PlayerView : ScenePanel
+{
+	public SceneModel Model { get; set; }
+
+	public PlayerView()
+	{
+		World = new SceneWorld();
+		var pawn = (Lord)Game.LocalPawn;
+
+		Model = new SceneModel( World, pawn.Model, pawn.Transform );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( !IsVisible ) return;
+
+		Model.Update( Time.Delta );
+
+		Camera.Position = Sandbox.Camera.Main.Position;
+		Camera.Rotation = Sandbox.Camera.Main.Rotation;
+	}
+}
