fff22c7a2c71a22012a66b8523ad5d787dcb4eee
yuberee <59583743+yuberee@users.noreply.github.com>
2023-03-31 22:01:56 +0200

    Use FBM for town generation, spawn houses first

--- code/Levels/TownGenerator.cs
+++ code/Levels/TownGenerator.cs
@@ -8,7 +8,12 @@ public class Town
 	public Vector3 Position { get; set; } = Vector3.Zero;
 	public Vector2 Bounds { get; set; } = Vector2.Zero;
 	public int Seed => TownSize.GetHashCode();
-	internal List<BaseEntity> townEntities = new();
+	internal List<Entity> townEntities = new();
+
+	public static Dictionary<string, float> PlaceableHouses { get; set; } = new()
+	{
+		{ "models/houses/house_a.vmdl", 1f },
+	};
 
 	public static Dictionary<string, float> PlaceableProps { get; set; } = new()
 	{
@@ -27,14 +32,50 @@ public class Town
 
 	public Town() { }
 
-	public float PerlinValue( float x = 0f, float y = 0f, float scale = 10f )
+	public float NoiseValue( float x = 0f, float y = 0f, float scale = 10f )
 	{
-		return Noise.Perlin( Seed / 1000f + x / scale, Seed / 1000f + y / scale );
+		return Noise.Fbm( 2, Seed / 100f + x / scale, Seed / 100f + y / scale );
 	}
 
-	internal async Task<bool> TryForProp( Random rand, Vector3 position, float x, float y, float density, float threshold )
+	internal async Task<bool> PlaceHouses( Random rand, Vector3 position, float x, float y, float density, Vector2 threshold )
 	{
-		if ( Current.PerlinValue( x, y ) <= threshold )
+		var noise = NoiseValue( x, y );
+		if ( noise >= threshold.x && noise <= threshold.y )
+		{
+			var spawnedHouse = new ModelEntity( WeightedList.RandomKey( rand, PlaceableHouses ) );
+			spawnedHouse.SetupPhysicsFromModel( PhysicsMotionType.Keyframed );
+
+			await GameTask.RunInThreadAsync( () =>
+			{
+				var randomOffsetX = (float)(rand.NextDouble() * 2f - 0.5f) * (50f / density);
+				var randomOffsetY = (float)(rand.NextDouble() * 2f - 0.5f) * (50f / density);
+				spawnedHouse.Position = position + new Vector3( x + randomOffsetX, y + randomOffsetY, 0 );
+				spawnedHouse.Rotation = Rotation.LookAt( spawnedHouse.Position - position);
+				var traceCheck = Trace.Body( spawnedHouse.PhysicsBody, spawnedHouse.Position )
+				.Ignore( spawnedHouse )
+				.EntitiesOnly()
+				.Run();
+
+				if ( traceCheck.Hit )
+				{
+					spawnedHouse.Delete();
+					return false;
+				}
+				else
+				{
+					Current.townEntities.Add( spawnedHouse );
+					return true;
+				}
+			} );
+		}
+
+		return false;
+	}
+
+	internal async Task<bool> TryForProp( Random rand, Vector3 position, float x, float y, float density, Vector2 threshold )
+	{
+		var noise = NoiseValue( x, y );
+		if ( noise >= threshold.x && noise <= threshold.y )
 		{
 			var spawnedProp = BaseProp.FromPrefab( WeightedList.RandomKey( rand, PlaceableProps ) );
 
@@ -44,18 +85,32 @@ public class Town
 				var randomOffsetY = (float)(rand.NextDouble() * 2f - 0.5f) * (50f / density);
 				spawnedProp.Position = position + new Vector3( x + randomOffsetX, y + randomOffsetY, 0 );
 				spawnedProp.Rotation = Rotation.FromYaw( rand.Next( 360 ) );
-				Current.townEntities.Add( spawnedProp );
-			} );
 
-			return true;
+				var traceCheck = Trace.Body( spawnedProp.PhysicsBody, spawnedProp.Position )
+				.Ignore( spawnedProp )
+				.EntitiesOnly()
+				.Run();
+
+				if ( traceCheck.Hit )
+				{
+					spawnedProp.Delete();
+					return false;
+				}
+				else
+				{
+					Current.townEntities.Add( spawnedProp );
+					return true;
+				}
+			} );
 		}
 
 		return false;
 	}
 
-	internal async Task<bool> TryForNPCs( Random rand, Vector3 position, float x, float y, float density, float threshold )
+	internal async Task<bool> TryForNPCs( Random rand, Vector3 position, float x, float y, float density, Vector2 threshold )
 	{
-		if ( Current.PerlinValue( x, y ) <= threshold )
+		var noise = NoiseValue( x, y );
+		if ( noise >= threshold.x && noise <= threshold.y )
 		{
 			var spawnedNPC = BaseNPC.FromPrefab( WeightedList.RandomKey( rand, PlaceablePeople ) );
 
@@ -65,6 +120,7 @@ public class Town
 				var randomOffsetY = (float)(rand.NextDouble() * 2f - 0.5f) * (50f / density);
 				spawnedNPC.Position = position + new Vector3( x + randomOffsetX, y + randomOffsetY, 0 );
 				spawnedNPC.Rotation = Rotation.FromYaw( rand.Next( 360 ) );
+
 				Current.townEntities.Add( spawnedNPC );
 			} );
 
@@ -74,7 +130,7 @@ public class Town
 		return false;
 	}
 
-	public static async void GenerateTown( Vector3 position, float townSize = 100f, float density = 3f )
+	public static async void GenerateTown( Vector3 position, float townSize, float density )
 	{
 		Current?.DeleteTown();
 
@@ -88,10 +144,21 @@ public class Town
 		{
 			for ( float y = -townWidth; y <= townWidth; y += 100f / density )
 			{
-				if ( await Current.TryForProp( rand, position, x, y, density, 0.4f ) )
+
+				if ( await Current.PlaceHouses( rand, position, x, y, density, new Vector2( 0f, 0.35f ) ) )
+					continue;
+			}
+		}
+
+		for ( float x = -townWidth; x <= townWidth; x += 100f / density )
+		{
+			for ( float y = -townWidth; y <= townWidth; y += 100f / density )
+			{
+
+				if ( await Current.TryForProp( rand, position, x, y, density * 2, new Vector2( 0.4f, 0.48f ) ) )
 					continue;
 
-				if ( await Current.TryForNPCs( rand, position, x, y, density, 0.45f ) )
+				if ( await Current.TryForNPCs( rand, position, x, y, density, new Vector2( 0.650f, 0.90f ) ) )
 					continue;
 			}
 		}
@@ -125,7 +192,7 @@ public class Town
 	}
 
 	[ConCmd.Admin( "town" )]
-	public static void CreateTown( float townSize = 100f, float density = 1f )
+	public static void CreateTown( float townSize = 100f, float density = 2f )
 	{
 		if ( ConsoleSystem.Caller.Pawn is not Lord player )
 			return;
