8b76078e432e07d9c64b29da15640612b8d49298
ceitine <fluffy00gt@gmail.com>
2023-03-31 19:02:21 +0300

    fully implement  Andrew Tate & racism

--- code/Levels/TownGenerator.cs
+++ code/Levels/TownGenerator.cs
@@ -14,7 +14,7 @@ public class Town
 	public int Seed => TownSize.GetHashCode();
 	internal List<BaseEntity> townEntities = new();
 
-	public static Dictionary<string,float> PlaceableProps { get; set; } = new()
+	public static Dictionary<string, float> PlaceableProps { get; set; } = new()
 	{
 		{ "prefabs/props/barrel.prefab", 2f },
 		{ "prefabs/props/largecrate.prefab", 2f },
@@ -29,7 +29,6 @@ public class Town
 		{ "prefabs/npcs/villager.prefab", 6f },
 	};
 
-
 	public Town() { }
 
 	public float PerlinValue( float x = 0f, float y = 0f, float scale = 10f )
@@ -37,17 +36,20 @@ public class Town
 		return Noise.Perlin( Seed / 1000f + x / scale, Seed / 1000f + y / scale );
 	}
 
-	internal bool TryForProp( Random rand, Vector3 position, float x, float y, float density, float threshold )
+	internal async Task<bool> TryForProp( Random rand, Vector3 position, float x, float y, float density, float threshold )
 	{
 		if ( Current.PerlinValue( x, y ) <= threshold )
 		{
 			var spawnedProp = BaseProp.FromPrefab( WeightedList.RandomKey( rand, PlaceableProps ) );
 
-			var randomOffsetX = (float)(rand.NextDouble() * 2f - 0.5f) * (50f / density);
-			var randomOffsetY = (float)(rand.NextDouble() * 2f - 0.5f) * (50f / density);
-			spawnedProp.Position = position + new Vector3( x + randomOffsetX, y + randomOffsetY, 0 );
-			spawnedProp.Rotation = Rotation.FromYaw( rand.Next( 360 ) );
-			Current.townEntities.Add( spawnedProp );
+			await GameTask.RunInThreadAsync( () =>
+			{
+				var randomOffsetX = (float)(rand.NextDouble() * 2f - 0.5f) * (50f / density);
+				var randomOffsetY = (float)(rand.NextDouble() * 2f - 0.5f) * (50f / density);
+				spawnedProp.Position = position + new Vector3( x + randomOffsetX, y + randomOffsetY, 0 );
+				spawnedProp.Rotation = Rotation.FromYaw( rand.Next( 360 ) );
+				Current.townEntities.Add( spawnedProp );
+			} );
 
 			return true;
 		}
@@ -55,17 +57,20 @@ public class Town
 		return false;
 	}
 
-	internal bool TryForNPCs( Random rand, Vector3 position, float x, float y, float density, float threshold )
+	internal async Task<bool> TryForNPCs( Random rand, Vector3 position, float x, float y, float density, float threshold )
 	{
 		if ( Current.PerlinValue( x, y ) <= threshold )
 		{
 			var spawnedNPC = BaseNPC.FromPrefab( WeightedList.RandomKey( rand, PlaceablePeople ) );
 
-			var randomOffsetX = (float)(rand.NextDouble() * 2f - 0.5f) * (50f / density);
-			var randomOffsetY = (float)(rand.NextDouble() * 2f - 0.5f) * (50f / density);
-			spawnedNPC.Position = position + new Vector3( x + randomOffsetX, y + randomOffsetY, 0 );
-			spawnedNPC.Rotation = Rotation.FromYaw( rand.Next( 360 ) );
-			Current.townEntities.Add( spawnedNPC );
+			await GameTask.RunInThreadAsync( () =>
+			{
+				var randomOffsetX = (float)(rand.NextDouble() * 2f - 0.5f) * (50f / density);
+				var randomOffsetY = (float)(rand.NextDouble() * 2f - 0.5f) * (50f / density);
+				spawnedNPC.Position = position + new Vector3( x + randomOffsetX, y + randomOffsetY, 0 );
+				spawnedNPC.Rotation = Rotation.FromYaw( rand.Next( 360 ) );
+				Current.townEntities.Add( spawnedNPC );
+			} );
 
 			return true;
 		}
@@ -79,45 +84,36 @@ public class Town
 
 		Current = new Town();
 		Current.TownSize = townSize;
+
 		var rand = new Random( Current.Seed );
+		var townWidth = 300f * (1f + townSize / 50f);
 
-		float townWidth = 300f * (1f + townSize / 50f);
-
-		var delayCounter = 0;
-		
-		for( float x = -townWidth; x <= townWidth; x += 100f / density )
+		for ( float x = -townWidth; x <= townWidth; x += 100f / density )
 		{
 			for ( float y = -townWidth; y <= townWidth; y += 100f / density )
 			{
-				if ( delayCounter >= 10 )
-				{
-					delayCounter = 0;
-					await GameTask.Delay( 1 );
-				}
-				if ( Current.TryForProp( rand, position, x, y, density, 0.4f ) )
-				{
-					delayCounter++;
+				if ( await Current.TryForProp( rand, position, x, y, density, 0.4f ) )
 					continue;
-				}
-				if ( Current.TryForNPCs( rand, position, x, y, density, 0.45f ) )
-				{
-					delayCounter++;
+
+				if ( await Current.TryForNPCs( rand, position, x, y, density, 0.45f ) )
 					continue;
-				}
 			}
 		}
 
-		Vector2 minBounds = new();
-		Vector2 maxBounds = new();
+		var minBounds = new Vector2();
+		var maxBounds = new Vector2();
 
-		foreach( var entity in Current.townEntities )
+		foreach ( var entity in Current.townEntities )
 		{
 			if ( entity.Position.x - position.x < minBounds.x )
 				minBounds.x = entity.Position.x;
+
 			if ( entity.Position.y - position.y < minBounds.y )
 				minBounds.y = entity.Position.y;
+
 			if ( entity.Position.x - position.x > maxBounds.x )
 				maxBounds.x = entity.Position.x;
+
 			if ( entity.Position.y - position.y > maxBounds.y )
 				maxBounds.y = entity.Position.y;
 		}
@@ -127,19 +123,16 @@ public class Town
 
 	public void DeleteTown()
 	{
-		foreach( var entity in townEntities )
-		{
+		foreach ( var entity in townEntities )
 			entity.Delete();
-		}
 	}
-
 
 	[ConCmd.Admin( "town" )]
 	public static void CreateTown( float townSize = 100f, float density = 1f )
 	{
-		var player = ConsoleSystem.Caller.Pawn as Lord;
+		if ( ConsoleSystem.Caller.Pawn is not Lord player )
+			return;
 
 		Town.GenerateTown( player.Position, townSize, density );
-
 	}
 }
