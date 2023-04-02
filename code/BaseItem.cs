f85e1295dca0fd026c1975bdeed27612e305c535
yuberee <59583743+yuberee@users.noreply.github.com>
2023-04-02 23:22:13 +0200

    BaseItem, club, helmet

---
+++ code/BaseItem.cs
@@ -0,0 +1,34 @@
+﻿namespace GameJam;
+
+public enum ItemType
+{
+	None,
+	Armor,
+	Weapon
+}
+
+[Prefab, Category("Items")]
+public partial class BaseItem : BaseEntity
+{
+
+	[Prefab, Category( "Stats" )]
+	public float IncreasedAttack { get; set; } = 0f;
+	[Prefab, Category( "Stats" )]
+	public float IncreasedHealth { get; set; } = 0f;
+	[Prefab, Category( "Stats" )]
+	public ItemType Type { get; set; } = ItemType.None;
+	public bool Equipped = false;
+
+	public override float GetWidth() => 20f;
+	public override float GetHeight() => 10f;
+
+	public static BaseItem FromPrefab( string prefabName )
+	{
+		if ( PrefabLibrary.TrySpawn<BaseItem>( prefabName, out var item ) )
+		{
+			return item;
+		}
+
+		return null;
+	}
+}
