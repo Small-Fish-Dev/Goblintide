63faf1c726166dad428e1a9a6fa9aaf51676fb14
lotuspar <91263142+lotuspar@users.noreply.github.com>
2023-04-03 03:11:16 +0800

    Upgrade positioning, hotloading creator

--- code/Player/UpgradeSystem/Creator.cs
+++ code/Player/UpgradeSystem/Creator.cs
@@ -2,13 +2,23 @@
 
 public static class Creator
 {
-	static Creator()
+	static Creator() => Build();
+
+	[Event.Hotload]
+	private static void Build()
 	{
+		Upgrade.ClearAll();
 		new Upgrade.Builder( "Aura of Fear I" )
-			.ConfigureWith( v => v.DecreasedAreaDiligence = 0.04f )
-			.Next( "Aura of Fear II" )
-			.Next( "Aura of Fear III" )
-			.Next( "Aura of Fear IV" )
+			.ConfigureWith( v =>
+			{
+				v.DecreasedAreaDiligence = 0.04f;
+			} )
+			.Next( "Aura of Fear II",
+				v => v.PlaceAt( Vector2.Down * 128 ) )
+			.Next( "Aura of Fear III",
+				v => v.PlaceAt( Vector2.Down * 256 ) )
+			.Next( "Aura of Fear IV",
+				v => v.PlaceAt( Vector2.Down * 400 ) )
 			.Build();
 	}
 }
