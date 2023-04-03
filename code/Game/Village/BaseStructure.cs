b1f9da238a796806b75e10bb8ec16860a9585563
ceitine <fluffy00gt@gmail.com>
2023-04-03 22:09:12 +0300

    implement base for building and structures, persist them

--- code/Game/Village/BaseStructure.cs
+++ code/Game/Village/BaseStructure.cs
@@ -3,4 +3,34 @@
 [Prefab]
 public partial class BaseStructure : ModelEntity
 {
+	public BuildingEntry Entry { get; set; }
+
+	public override void Spawn()
+	{
+		SetupPhysicsFromModel( PhysicsMotionType.Static );
+
+		foreach ( var component in Components.GetAll<StructureComponent>() )
+			component.Initialize();
+	}
+
+	public override void ClientSpawn()
+	{
+		foreach ( var component in Components.GetAll<StructureComponent>() )
+			component.Initialize();
+	}
+
+	protected override void OnDestroy()
+	{
+		foreach ( var component in Components.GetAll<StructureComponent>() )
+			component.OnDestroy();
+	}
+
+	public static BaseStructure FromPrefab( string prefabName )
+	{
+		var path = $"prefabs/structures/{prefabName}.prefab";
+		if ( PrefabLibrary.TrySpawn<BaseStructure>( path, out var structure ) )
+			return structure;
+
+		return null;
+	}
 }
