4fe9b5f25ef95215beddb405793f2573363b5f5a
ceitine <fluffy00gt@gmail.com>
2023-04-03 16:21:36 +0300

    get started with village structure stuff

---
+++ code/Game/Village/Components/StructureComponent.cs
@@ -0,0 +1,19 @@
+﻿namespace GameJam;
+
+public partial class StructureComponent : EntityComponent<BaseStructure>
+{
+	/// <summary>
+	/// The ItemPrefab entity this component is attached to.
+	/// </summary>
+	public BaseStructure Structure => Entity;
+
+	/// <summary>
+	/// Called when the BaseStructure constructor is called.
+	/// </summary>
+	public virtual void Initialize() { }
+
+	/// <summary>
+	/// Called when the BaseStructure is destroyed.
+	/// </summary>
+	public virtual void OnDestroy() { }
+}
