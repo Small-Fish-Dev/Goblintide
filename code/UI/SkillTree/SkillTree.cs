abceb08f35e0957890165d8bf5022f1380423d91
lotuspar <91263142+lotuspar@users.noreply.github.com>
2023-04-05 23:50:54 +0800

    Dragging support in SkillTree, uses PanelCamera

--- code/UI/SkillTree/SkillTree.cs
+++ code/UI/SkillTree/SkillTree.cs
@@ -29,4 +29,46 @@ public partial class SkillTree
 		// Set initial selected actor
 		Select( actors.FirstOrDefault() );
 	}
+
+	private bool _dragging;
+	private Vector2 _lastMousePos;
+
+	/// <summary> Whether or not the world map can be translated </summary>
+	private static bool CanStartDragging( Panel target )
+	{
+		var attribute = target.GetAttribute( "allow-translate" );
+		return attribute != null;
+	}
+
+	protected override void OnMouseDown( MousePanelEvent e )
+	{
+		base.OnMouseDown( e );
+
+		if ( !CanStartDragging( e.Target ) ) return;
+
+		if ( e.MouseButton != MouseButtons.Left ) return;
+
+		ClearSelection();
+
+		_dragging = true;
+	}
+
+	protected override void OnMouseUp( MousePanelEvent e )
+	{
+		base.OnMouseUp( e );
+
+		_dragging = false;
+	}
+
+	protected override void OnMouseMove( MousePanelEvent e )
+	{
+		base.OnMouseMove( e );
+
+		if ( _dragging )
+		{
+			Camera.Position += (MousePosition - _lastMousePos) * ScaleFromScreen;
+		}
+
+		_lastMousePos = MousePosition;
+	}
 }
