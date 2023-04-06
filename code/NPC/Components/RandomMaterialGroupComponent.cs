7a85a6e30d3885a2f0a77aa02e94680bc3588a72
Luke T <luke.tedd@gmail.com>
2023-04-07 00:41:06 +0100

    allow to deal with specific randomized material groups with weights

--- code/NPC/Components/RandomMaterialGroupComponent.cs
+++ code/NPC/Components/RandomMaterialGroupComponent.cs
@@ -9,8 +9,23 @@ namespace GameJam;
 [Prefab]
 public partial class RandomMaterialGroupComponent : EntityComponent<ModelEntity>
 {
+	[Prefab]
+	public virtual bool WeightedMaterialGroups { get; set; } = false;
+
+	[Prefab]
+	public virtual Dictionary<int, float> MaterialGroupsAndWeight { get; set; }
 	protected override void OnActivate()
 	{
-		Entity.SetMaterialGroup( new Random().Int( 0, Entity.MaterialGroupCount ) );
+		if ( !Game.IsServer ) return;
+
+		int MaterialGroup = 0;
+
+		if ( WeightedMaterialGroups && MaterialGroupsAndWeight.Count > 0 )
+		{
+			MaterialGroup = WeightedList.RandomKey( MaterialGroupsAndWeight );
+			Log.Info( $"Material group {MaterialGroup} was chosen" );
+		}
+		else MaterialGroup = new Random().Int( 0, Entity.MaterialGroupCount );
+		Entity.SetMaterialGroup( MaterialGroup );
 	}
 }
