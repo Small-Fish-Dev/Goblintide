10fb4a28bc57c729dbead4af068c9a39b8942aad
lotuspar <91263142+lotuspar@users.noreply.github.com>
2023-04-09 05:08:08 +0800

    ui: fix `Image` position / sizing

--- code/Graphicator/Items/Image.cs
+++ code/Graphicator/Items/Image.cs
@@ -7,12 +7,26 @@ public class Image : GraphicsItem
 	public Texture Texture;
 
 	public ImageRendering Filtering = ImageRendering.Anisotropic;
+	public BackgroundRepeat Repeating = BackgroundRepeat.NoRepeat;
 
 	public override void Render()
 	{
 		var attributes = Graphics.Attributes;
+
+		var rect = ScreenspaceRect;
+
 		attributes.Set( "Texture", Texture ?? Texture.Invalid );
+		attributes.Set( "HasBorder", 0 );
+
+		attributes.Set( "BgRepeat", (int)Repeating );
+		attributes.Set( "BgPos", new Vector4( 0, 0, rect.Width, rect.Height ) );
+
 		attributes.Set( "D_TEXTURE_FILTERING", (int)Filtering );
-		Graphics.DrawQuad( ScreenspaceRect, Material.UI.Box, Color.Transparent, attributes );
+		attributes.Set( "D_BACKGROUND_IMAGE", 1 );
+
+		attributes.Set( "BoxPosition", rect.TopLeft );
+		attributes.Set( "BoxSize", rect.Size );
+
+		Graphics.DrawQuad( ScreenspaceRect, Material.UI.Box, Color.Blue, attributes );
 	}
 }
